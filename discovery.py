d2f5b58bbb1e8e64842ca8645c6c1be63cf64648
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2022-01-28 13:20:59 -0500

    Update discovery.py
    made more Python3

--- discovery.py
+++ discovery.py
@@ -3,10 +3,14 @@
 #
 from socket import socket, AF_INET, SOCK_DGRAM
 
+PORT = 50000
+MAGIC = bytes('xu49d",'utf-8')
+
 s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
 s.bind(('', PORT))
 
 while 1:
     data, addr = s.recvfrom(1024) #wait for a packet
     if data.startswith(MAGIC):
-        print "got service announcement from", data[len(MAGIC):]
+        print("got service announcement from", data[len(MAGIC):])
+              
