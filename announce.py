42b7a25d2b3643c3a0c0f29641faf76b63c0eba6
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2022-01-28 13:22:45 -0500

    Update announce.py
    basic update to Python3

--- announce.py
+++ announce.py
@@ -4,6 +4,10 @@
 from time import sleep
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, gethostbyname, gethostname
 
+
+PORT = 50000
+MAGIC = bytes('xu49d",'utf-8')
+
 s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
 s.bind(('', 0))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #this is a broadcast socket
@@ -12,5 +16,5 @@ my_ip= gethostbyname(gethostname()) #get our IP. Be careful if you have multiple
 while 1:
     data = MAGIC+my_ip
     s.sendto(data, ('<broadcast>', PORT))
-    print "sent service announcement"
+    print ("sent service announcement")
     sleep(5)
